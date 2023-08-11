adade62a51176f5eabd2381b13a680b62d1481c0
Rushikesh Sakharwade <rushikesh.sakharwade@livehealth.in>
2023-08-11 11:11:54 +0530

    config changes

--- lims_demo_backend/settings.py
+++ lims_demo_backend/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'lims_demo_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
